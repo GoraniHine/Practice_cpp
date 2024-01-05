c3be1cefc3ce51d5bb60ec0e465de8de6ca4d936
Choi minsik <128362719+GoraniHine@users.noreply.github.com>
2024-01-06 06:18:26 +0900

    240106

---
+++ Basic_Class/Class/Inline/RacingInlineMain.cpp
@@ -0,0 +1,16 @@
+#include "CarInline.h"
+#include "CarInline.cpp"
+
+int main(void)
+{
+    Car run99;
+    run99.InitMembers("run99", 100);
+    run99.Accel();
+    run99.Accel();
+    run99.Accel();
+    run99.ShowCarState();
+    run99.Break();
+    run99.ShowCarState();
+
+    return 0;
+}
