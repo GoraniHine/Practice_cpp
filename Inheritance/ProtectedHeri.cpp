f87af228c5c49030543296de1e62326ffeb707b0
Choi minsik <128362719+GoraniHine@users.noreply.github.com>
2024-01-15 09:50:58 +0900

    240115

---
+++ Inheritance/ProtectedHeri.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+    private:
+        int num1;
+    
+    protected:
+        int num2;
+
+    public:
+        int num3;
+
+        Base(): num1(1), num2(2), num3(3){}
+        
+};
+
+class Derived: protected Base{};
+
+int main(void)
+{
+    Derived drv;
+    // cout << drv.num3 << endl; 컴파일 에러 발생 
+    return 0;
+}
